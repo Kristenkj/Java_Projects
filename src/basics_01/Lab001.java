ed3826cb213e92c16d7ea8494d1eec73cf905e62
Kristenkj <jonesbkristen@gmail.com>
2024-01-04 16:04:02 -0500

    Initial commit

---
+++ src/basics_01/Lab001.java
@@ -0,0 +1,13 @@
+package src.basics_01;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("Hello WORLD!!!");
+
+        //Single comment
+        /**
+         multi line command
+         Author - Kristen
+         **/
+    }
+}
---
+++ src/basics_01/Lab002.java
@@ -0,0 +1,9 @@
+package src.basics_01;
+
+public class Lab002 {
+    public static void main(String[] args) {
+    short age = 130;
+
+    System.out.println (age);
+    }
+}
