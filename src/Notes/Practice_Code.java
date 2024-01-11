420a8153416e65ae00beec30902968e788309154
Kristenkj <jonesbkristen@gmail.com>
2024-01-11 02:59:31 -0500

    Commit 1/11/2024

---
+++ src/Notes/Practice_Code.java
@@ -0,0 +1,8 @@
+package src.Notes;
+
+public class Practice_Code {
+    public static void main(String[] args) {
+        //This project file includes notes and practice
+        System.out.println("Success");
+    }
+}
