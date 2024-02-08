3bdb23612b84bcc0e58d54f7470f4d9afd57f7f1
Kristenkj <jonesbkristen@gmail.com>
2024-02-08 03:39:29 -0500

    Commit 2/8/2024

--- Games/Droid.java
+++ Games/Droid.java
@@ -1,7 +1,36 @@
 package Games;
 
 public class Droid {
+
+    String name;
+    int batteryLevel;
+
+    public Droid(String droidName) {
+
+        batteryLevel = 100;
+        name = droidName;
+    }
+
+    public void performTask(String task) {
+        //codey.performTask("Singing");
+        System.out.println(name + " is performing task: " + task);
+        batteryLevel = batteryLevel - 10;
+    }
+
+    public String toString() {
+        return "Hello, I’m the droid:" + name;
+    }
+
     public static void main(String[] args) {
 
+        Droid codey = new Droid("Codey");
+        System.out.println(codey);
+        //Performing Task
+        codey.performTask("dancing");
+        codey.performTask("coding");
+
+
+
     }
+
 }
