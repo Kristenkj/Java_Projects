caa063f70fe57fff395c1bea162747f5de467abc
Kristenkj <jonesbkristen@gmail.com>
2024-03-04 09:56:57 -0500

    Commit 3/4/2024

---
+++ Challenges/hr30Days/day2.java
@@ -0,0 +1,33 @@
+package Challenges.hr30Days;
+
+import java.util.Scanner;
+import java.io.*;
+public class day2 {
+
+    public static void main(String[] args) {
+
+        int i = 4;
+        double d = 4.0;
+        String s1 = "HackerRank";
+
+        Scanner sc = new Scanner(System.in);
+        //Numerical Input
+        int a = sc.nextInt();
+        double c = sc.nextDouble();
+
+        //String Input
+        sc.nextLine();
+        String s2 = sc.nextLine();
+        String s3 = STR."\{s1} \{s2}";
+
+
+        System.out.println(a + i);
+        System.out.println(c + d);
+        System.out.println(s3);
+
+    }
+
+
+}
+
+
